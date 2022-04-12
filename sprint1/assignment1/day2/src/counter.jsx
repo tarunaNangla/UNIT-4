3acd99b97b3f34d77900a42c0e51e4880da2805f
tarunaNangla <tarunanangla131@gmail.com>
2022-04-12 17:21:26 +0530

    change
    change

---
+++ sprint1/assignment1/day2/src/counter.jsx
@@ -0,0 +1,36 @@
+
+ import React,{useState} from "react";
+
+const Counter = ()=>
+{
+
+    const[count,setCount] = useState(0);
+    
+    
+    const handleinc = () => {
+        setCount((prevState)=>prevState+1);
+    }
+   
+    const handledec = () => {
+        setCount(count-1);
+    }
+   
+    const handle = () => {
+        setCount(count*2);
+    }
+
+    return(
+<>
+
+          <h1>{count}</h1>
+          <button onClick={() => handleinc()}>Increment</button>
+          <button onClick={() => handledec()}>Decrement</button>
+          <button onClick={() => handle()}>Double</button>
+
+
+
+</>
+    )
+}
+
+export default Counter;
