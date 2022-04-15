1a253a6fbedba15cf7ce4e4c11544035a0a6a6c8
tarunaNangla <tarunanangla131@gmail.com>
2022-04-15 19:26:46 +0530

    problem 2
    todo data

---
+++ sprint1/assignment1/day3/src/problem2/Todo.jsx
@@ -0,0 +1,53 @@
+import { useState } from "react";
+import TaskItem from "./TodoList";
+
+
+function Todo ()
+{
+
+    const [query,SetQuery]=useState("");
+    const [tasks,SetTasks]=useState([]);
+
+    const handlequ = (e)=>{
+
+        let {value}=e.target;
+        SetQuery(value);
+        console.log(value)
+    }
+
+    const handleAdd = () =>{
+        let pyload={
+            title:query
+        }
+       let newTasks=[...tasks,pyload];
+       SetTasks(newTasks);
+       console.log(newTasks)
+
+    }
+    return(
+        <>
+        <div>Tasks</div>
+
+         <div>
+
+             {/* < TodoList {...tasks}/> */}
+             {
+                 tasks.map((ele)=>{
+                     return(
+                         
+                         <TaskItem title={ele.title} />
+                         )
+                        })
+                    }
+         </div>
+                    <input value={query} onChange={handlequ} placeholder='add details'></input>
+            
+                    <button onClick={handleAdd}>Add</button>
+
+        </>
+    )
+ 
+}
+
+
+export default Todo;
---
+++ sprint1/assignment1/day3/src/problem2/TodoList.jsx
@@ -0,0 +1,13 @@
+
+
+function TaskItem(props){
+    // console.log(props)
+    return(
+        <div className="square">
+       <h1>{props.title}</h1>
+        </div>
+    )
+}
+
+
+export default TaskItem;
